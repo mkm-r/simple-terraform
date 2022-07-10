5b68c7b5ea88e4acd4c18e773306fcd317db56b4
MKM <mujeebur.rahman@in.ibm.com>
2022-07-10 16:39:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@
 
 resource "null_resource" "test" {
    provisioner "local-exec" {
-    command = "dnf install -y iputils"
+    command = "curl -v https://gitlab.mgmt.kdorigin-st.nissay.co.jp"
      }
   
   }
